efde36bd648437a3397529bfd39a2df15451d06e
Soumydeep2308 <87338582+Soumydeep2308@users.noreply.github.com>
2022-01-11 06:10:56 +0530

    apocalypse

---
+++ JS/Directors.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  $(".F1").click(function(){
+    $(".para-1").removeClass("None");
+    $(".FF1").removeClass("none");
+    $(".para-1").addClass("OPACITY");
+    $(".FF1").addClass("OPACITY-B");
+  });
+});
+
+$(document).ready(function(){
+  $(".FF1").click(function(){
+    $(".para-1").addClass("None");
+    $(".FF1").removeClass("OPACITY-B");
+    $(".FF1").addClass("none");
+    $(".para-1").removeClass("OPACITY");
+  });
+});
+
+$(document).ready(function(){
+  $(".F2").click(function(){
+    $(".para-2").removeClass("None");
+    $(".FF2").removeClass("none");
+    $(".para-2").addClass("OPACITY");
+    $(".FF2").addClass("OPACITY-B");
+  });
+});
+
+$(document).ready(function(){
+  $(".FF2").click(function(){
+    $(".para-2").addClass("None");
+    $(".FF2").removeClass("OPACITY-B");
+    $(".FF2").addClass("none");
+    $(".para-2").removeClass("OPACITY");
+  });
+});
+
+$(document).ready(function(){
+  $(".F3").click(function(){
+    $(".para-3").removeClass("None");
+    $(".FF3").removeClass("none");
+    $(".para-3").addClass("OPACITY");
+    $(".FF3").addClass("OPACITY-B");
+  });
+});
+
+$(document).ready(function(){
+  $(".FF3").click(function(){
+    $(".para-3").addClass("None");
+    $(".FF3").removeClass("OPACITY-B");
+    $(".FF3").addClass("none");
+    $(".para-3").removeClass("OPACITY");
+  });
+});
+
+$(document).ready(function(){
+  $(".F4").click(function(){
+    $(".para-4").removeClass("None");
+    $(".FF4").removeClass("none");
+    $(".para-4").addClass("OPACITY");
+    $(".FF4").addClass("OPACITY-B");
+  });
+});
+
+$(document).ready(function(){
+  $(".FF4").click(function(){
+    $(".para-4").addClass("None");
+    $(".FF4").removeClass("OPACITY-B");
+    $(".FF4").addClass("none");
+    $(".para-4").removeClass("OPACITY");
+  });
+});
---
+++ JS/our-team.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  $(".p1").click(function(){
+    $(".pp1").removeClass("None");
+    $(".pp1").addClass("Block");
+    $(".p1").addClass("None");
+    $(".p11").removeClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p11").click(function(){
+    $(".pp1").addClass("None");
+    $(".pp1").removeClass("Block");
+    $(".p1").removeClass("None");
+    $(".p11").addClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p2").click(function(){
+    $(".pp2").removeClass("None");
+    $(".pp2").addClass("Block");
+    $(".p2").addClass("None");
+    $(".p22").removeClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p22").click(function(){
+    $(".pp2").addClass("None");
+    $(".pp2").removeClass("Block");
+    $(".p2").removeClass("None");
+    $(".p22").addClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p3").click(function(){
+    $(".pp3").removeClass("None");
+    $(".pp3").addClass("Block");
+    $(".p3").addClass("None");
+    $(".p33").removeClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p33").click(function(){
+    $(".pp3").addClass("None");
+    $(".pp3").removeClass("Block");
+    $(".p3").removeClass("None");
+    $(".p33").addClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p4").click(function(){
+    $(".pp4").removeClass("None");
+    $(".pp4").addClass("Block");
+    $(".p4").addClass("None");
+    $(".p44").removeClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p44").click(function(){
+    $(".pp4").addClass("None");
+    $(".pp4").removeClass("Block");
+    $(".p4").removeClass("None");
+    $(".p44").addClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p5").click(function(){
+    $(".pp5").removeClass("None");
+    $(".pp5").addClass("Block");
+    $(".p5").addClass("None");
+    $(".p55").removeClass("None");
+  });
+});
+
+$(document).ready(function(){
+  $(".p55").click(function(){
+    $(".pp5").addClass("None");
+    $(".pp5").removeClass("Block");
+    $(".p5").removeClass("None");
+    $(".p55").addClass("None");
+  });
+});
